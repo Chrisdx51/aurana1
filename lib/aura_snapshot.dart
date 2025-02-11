63057e38e251af64e6107e45c8135f2a889b30e8
Chrisdx51 <kristianx@live.co.uk>
2025-02-11 15:34:39 +0000

    Your commit message

---
+++ lib/aura_snapshot.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuraSnapshot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Aura Snapshot Feature Coming Soon",
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
